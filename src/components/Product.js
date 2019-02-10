275730e286abbe2db6fec173f880321c59c63ead
hoanganh <anhdvh@ows.com.vn>
2019-02-10 22:32:54 +0700

    show product

--- src/components/Product.js
+++ src/components/Product.js
@@ -42,10 +42,10 @@ class Product extends Component {
   showRating(rating) {
     let result = [];
     for (let i = 1; i <= rating; i++) {
-      result.push(<i className="fa fa-star" />);
+      result.push(<i key={i} className="fa fa-star" />);
     }
     for (let j = 1; j <= ( 5-rating); j++) {
-      result.push(<i className="fa fa-star-o" />);
+      result.push(<i key={j+10} className="fa fa-star-o" />);
     }
     return result;
   }
